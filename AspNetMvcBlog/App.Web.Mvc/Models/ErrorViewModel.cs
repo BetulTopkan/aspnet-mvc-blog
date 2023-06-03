b81d5fe5df1d9eb26ed01481eba3608bec926a6d
Betul Topkan <betultopkann@gmail.com>
2023-06-03 09:56:03 +0300

    convert net6 to net5

--- AspNetMvcBlog/App.Web.Mvc/Models/ErrorViewModel.cs
+++ AspNetMvcBlog/App.Web.Mvc/Models/ErrorViewModel.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace App.Web.Mvc.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
